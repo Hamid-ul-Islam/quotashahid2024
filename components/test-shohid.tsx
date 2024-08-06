41f3acf7e3d6d617bef0f549d0ea571941abb6d3
Hamid <hamidthedev@gmail.com>
2024-08-06 21:29:16 +0600

    shohid section fix

--- components/test-shohid.tsx
+++ components/test-shohid.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { shohidDB as allData } from '@/lib/data';
 import { useEffect, useRef, useState } from 'react';
+import Heading from './typography/heading';
+import Paragraph from './typography/paragraph';
 import SubHeading from './typography/sub-heading';
 
 interface DataItem {
@@ -75,31 +77,46 @@ const InfiniteScroll: React.FC = () => {
   }, [data, hasMore]);
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-5 gap-5">
-      {data.map((shohid, index) => (
-        <div key={index} className="rounded-xl p-3 bg-gray-50 border">
-          <div className="relative">
-            <img src={shohid?.imageUrl} alt={shohid?.name} />
-            <span className="absolute top-0 right-0 px-3 py-1 bg-red-900 text-white">
-              {index + 1}
+    <div className="mb-10">
+      <div>
+        <Heading className="">
+          List of <span className="text-red-900">Martyrs(Shahid)</span> in the
+          Movement
+        </Heading>
+        <Paragraph>
+          These martyrs did not just give their lives; they gifted us a future
+          free from the chains of autocracy. Their courage in the face of
+          overwhelming adversity serves as a beacon of hope and a reminder of
+          the power of unity and determination.
+        </Paragraph>
+      </div>
+      <hr className="my-5 border" />
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-5">
+        {data.map((shohid, index) => (
+          <div key={index} className="rounded-xl p-3 bg-gray-50 border">
+            <div className="relative">
+              <img src={shohid?.imageUrl} alt={shohid?.name} />
+              <span className="absolute top-0 right-0 px-3 py-1 bg-red-900 text-white">
+                {index + 1}
+              </span>
+            </div>
+            <div>
+              <SubHeading className="text-lg mt-3">
+                <span className="text-red-900">Shahid</span> {shohid?.name}
+              </SubHeading>
+              <p className="font-medium text-md">{shohid?.role}</p>
+              <p className="my-1">{shohid?.institution}</p>
+              <p>{shohid?.date}</p>
+            </div>
+          </div>
+        ))}
+        <div className="w-full p-10" ref={lastElementRef}>
+          {loading && (
+            <span className="text-red-900 font-bold text-2xl">
+              Loading more data...
             </span>
-          </div>
-          <div>
-            <SubHeading className="text-lg mt-3">
-              <span className="text-red-900">Shahid</span> {shohid?.name}
-            </SubHeading>
-            <p className="font-medium text-md">{shohid?.role}</p>
-            <p className="my-1">{shohid?.institution}</p>
-            <p>{shohid?.date}</p>
-          </div>
+          )}
         </div>
-      ))}
-      <div className="w-full p-10" ref={lastElementRef}>
-        {loading && (
-          <span className="text-red-900 font-bold text-2xl">
-            Loading more data...
-          </span>
-        )}
       </div>
     </div>
   );
